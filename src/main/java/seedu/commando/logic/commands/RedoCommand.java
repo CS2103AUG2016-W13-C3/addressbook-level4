82bb55cd2384b541bd50ea198ef54e7406b8ab99
zzzzwen <zhiwen.song.loveslife@gmail.com>
2016-10-16 02:20:24 +0800

    Add comment for redo and undo commands

--- src/main/java/seedu/commando/logic/commands/RedoCommand.java
+++ src/main/java/seedu/commando/logic/commands/RedoCommand.java
@@ -7,6 +7,9 @@ import seedu.commando.commons.core.Messages;
 import seedu.commando.model.Model;
 import seedu.commando.model.todo.ReadOnlyToDo;
 
+/**
+ *  Redo the last undo action
+ */
 public class RedoCommand extends Command {
     
     public static final String COMMAND_WORD = "redo";
@@ -14,11 +17,16 @@ public class RedoCommand extends Command {
     public RedoCommand(){
         
     }
-
+    
+    /**
+     * Asserts that {@code eventsCenter} and {@code model} are non-null
+     */
     @Override
     public CommandResult execute(List<ReadOnlyToDo> toDoAtIndices, Model model, EventsCenter eventsCenter) {
         assert model != null;
+        assert eventsCenter != null;
         
+        //Checks if such redo action can be executed
         if (model.redoToDoList()) {
             return new CommandResult(Messages.REDID_COMMAND);
         } else {
--- src/main/java/seedu/commando/logic/commands/UndoCommand.java
+++ src/main/java/seedu/commando/logic/commands/UndoCommand.java
@@ -18,10 +18,15 @@ public class UndoCommand extends Command{
         
     }
     
+    /**
+     * Asserts that {@code eventsCenter} and {@code model} are non-null
+     */
     @Override
     public CommandResult execute(List<ReadOnlyToDo> toDoAtIndices, Model model, EventsCenter eventsCenter) {
         assert model != null;
+        assert eventsCenter != null;
         
+        //check if there is undo action taken
         if (model.undoToDoList()) {
             return new CommandResult(Messages.UNDID_COMMAND);
         } else {
