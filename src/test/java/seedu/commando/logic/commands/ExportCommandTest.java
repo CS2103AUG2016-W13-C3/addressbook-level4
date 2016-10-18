c244c6be4541c2930fd5d5cdc0871011d6f29b14
zzzzwen <zhiwen.song.loveslife@gmail.com>
2016-10-18 12:31:03 +0800

    Remove try catch clause

--- src/test/java/seedu/commando/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/commando/logic/commands/ExportCommandTest.java
@@ -13,6 +13,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
 import javafx.scene.shape.Path;
@@ -26,58 +27,60 @@ import seedu.commando.storage.StorageManager;
 import seedu.commando.testutil.EventsCollector;
 
 public class ExportCommandTest {
-	@Rule
-	public TemporaryFolder folder = new TemporaryFolder();
-
-	private Model model;
-	private Logic logic;
-	private EventsCollector eventsCollector;
-	private File toDoListFile;
-	private File userPrefsFile;
-
-	@Before
-	public void setup() throws IOException {
-		model = new ModelManager();
-
-		toDoListFile = folder.newFile();
-		userPrefsFile = folder.newFile();
-		logic = new LogicManager(model,
-				new StorageManager(toDoListFile.getAbsolutePath(), userPrefsFile.getAbsolutePath()));
-
-		eventsCollector = new EventsCollector();
-	}
-
-	@After
-	public void teardown() {
-		EventsCenter.clearSubscribers();
-	}
-
-	@Test
-    public void execute_export_emptyPath() {
-    	CommandResult result = logic.execute("export");
-    	assertTrue(result.hasError());
-    	assertEquals(Messages.MISSING_EXPORT_PATH, result.getFeedback());
-    	logic.execute("export    ");
-    	assertTrue(result.hasError());
-    	assertEquals(Messages.MISSING_EXPORT_PATH, result.getFeedback());
-    }
-	
-	@Test
-	public void execute_export_invalidPath() {
-		CommandResult result = logic.execute("export test\\");
-    	assertTrue(result.hasError());
-    	assertEquals(Messages.MISSING_EXPORT_FILE, result.getFeedback());
-	}
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
     
-	@Test
-	public void execute_export_validPath() {
-	    logic.execute("add test 1");
-	    CommandResult result = logic.execute("export test");
-	    assertFalse(result.hasError());
-	    try {
-            assertTrue(Arrays.equals(Files.readAllBytes(toDoListFile.toPath()), Files.readAllBytes(Paths.get("test"))));
-            Files.delete(Paths.get("test"));
-        } catch (IOException e) {
-        }
-	}
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    private Model model;
+    private Logic logic;
+    private EventsCollector eventsCollector;
+    private File toDoListFile;
+    private File userPrefsFile;
+
+    @Before
+    public void setup() throws IOException {
+        model = new ModelManager();
+
+        toDoListFile = folder.newFile();
+        userPrefsFile = folder.newFile();
+        logic = new LogicManager(model,
+                new StorageManager(toDoListFile.getAbsolutePath(), userPrefsFile.getAbsolutePath()));
+
+        eventsCollector = new EventsCollector();
+    }
+
+    @After
+    public void teardown() {
+        EventsCenter.clearSubscribers();
+    }
+
+    @Test
+    public void execute_export_emptyPath() {
+        CommandResult result = logic.execute("export");
+        assertTrue(result.hasError());
+        assertEquals(Messages.MISSING_EXPORT_PATH, result.getFeedback());
+        logic.execute("export    ");
+        assertTrue(result.hasError());
+        assertEquals(Messages.MISSING_EXPORT_PATH, result.getFeedback());
+    }
+
+    @Test
+    public void execute_export_invalidPath() {
+        CommandResult result = logic.execute("export test\\");
+        assertTrue(result.hasError());
+        assertEquals(Messages.MISSING_EXPORT_FILE, result.getFeedback());
+    }
+
+    @Test
+    public void execute_export_validPath() throws IOException {
+        logic.execute("add test 1");
+        CommandResult result = logic.execute("export test");
+        assertFalse(result.hasError());
+        
+        thrown.expect(IOException.class);
+        assertTrue(Arrays.equals(Files.readAllBytes(toDoListFile.toPath()), Files.readAllBytes(Paths.get("test"))));
+        Files.delete(Paths.get("test"));
+    }
 }
--- src/test/java/seedu/commando/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/commando/logic/commands/ImportCommandTest.java
@@ -13,6 +13,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
 import seedu.commando.commons.core.EventsCenter;
@@ -28,6 +29,9 @@ public class ImportCommandTest {
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     private Model model;
     private Logic logic;
     private EventsCollector eventsCollector;
@@ -39,11 +43,9 @@ public class ImportCommandTest {
         model = new ModelManager();
 
         toDoListFile = folder.newFile();
-        userPrefsFile  = folder.newFile();
-        logic = new LogicManager(model, new StorageManager(
-            toDoListFile.getAbsolutePath(),
-            userPrefsFile.getAbsolutePath()
-        ));
+        userPrefsFile = folder.newFile();
+        logic = new LogicManager(model,
+                new StorageManager(toDoListFile.getAbsolutePath(), userPrefsFile.getAbsolutePath()));
 
         eventsCollector = new EventsCollector();
     }
@@ -52,54 +54,53 @@ public class ImportCommandTest {
     public void teardown() {
         EventsCenter.clearSubscribers();
     }
-    
+
     @Test
     public void execute_import_emptyPath() {
-    	CommandResult result = logic.execute("import");
-    	assertTrue(result.hasError());
-    	assertEquals(Messages.MISSING_IMPORT_PATH, result.getFeedback());
-    	result = logic.execute("import    ");
-    	assertTrue(result.hasError());
-    	assertEquals(Messages.MISSING_IMPORT_PATH, result.getFeedback());
+        CommandResult result = logic.execute("import");
+        assertTrue(result.hasError());
+        assertEquals(Messages.MISSING_IMPORT_PATH, result.getFeedback());
+        result = logic.execute("import    ");
+        assertTrue(result.hasError());
+        assertEquals(Messages.MISSING_IMPORT_PATH, result.getFeedback());
     }
-    
+
     @Test
     public void execute_import_invalidPath() {
-    	CommandResult result = logic.execute("import 2\\");
-    	assertTrue(result.hasError());
-    	assertEquals(Messages.MISSING_IMPORT_FILE, result.getFeedback());
-    	result = logic.execute("import awe@#$\\");
-    	assertTrue(result.hasError());
-    	assertEquals(Messages.MISSING_IMPORT_FILE, result.getFeedback());
-    	result = logic.execute("import 3");
-    	assertTrue(result.hasError());
-    	assertEquals(Messages.IMPORT_COMMAND_TYPE_ERROR, result.getFeedback());
-    	result = logic.execute("import 3.doc");
-    	assertTrue(result.hasError());
-    	assertEquals(Messages.IMPORT_COMMAND_TYPE_ERROR, result.getFeedback());
-    	result = logic.execute("import this cant be there.XMl");
-    	assertTrue(result.hasError());
-    	assertEquals(Messages.IMPORT_COMMAND_FILE_NOT_EXIST, result.getFeedback());
+        CommandResult result = logic.execute("import 2\\");
+        assertTrue(result.hasError());
+        assertEquals(Messages.MISSING_IMPORT_FILE, result.getFeedback());
+        result = logic.execute("import awe@#$\\");
+        assertTrue(result.hasError());
+        assertEquals(Messages.MISSING_IMPORT_FILE, result.getFeedback());
+        result = logic.execute("import 3");
+        assertTrue(result.hasError());
+        assertEquals(Messages.IMPORT_COMMAND_TYPE_ERROR, result.getFeedback());
+        result = logic.execute("import 3.doc");
+        assertTrue(result.hasError());
+        assertEquals(Messages.IMPORT_COMMAND_TYPE_ERROR, result.getFeedback());
+        result = logic.execute("import this cant be there.XMl");
+        assertTrue(result.hasError());
+        assertEquals(Messages.IMPORT_COMMAND_FILE_NOT_EXIST, result.getFeedback());
     }
-    
+
     @Test
-    public void execute_import_validPath(){
+    public void execute_import_validPath() throws IOException {
         logic.execute("add test1");
         logic.execute("add test2");
         logic.execute("export test.xml");
         logic.execute("clear");
         assertTrue(wasToDoListChangedEventPosted(eventsCollector));
         assertTrue(model.getToDoList().getToDos().size() == 0);
-        
+
         CommandResult result = logic.execute("import test.xml");
         assertFalse(result.hasError());
         assertTrue(wasToDoListChangedEventPosted(eventsCollector));
         assertTrue(model.getToDoList().getToDos().size() == 2);
         
-        try {
-            Files.delete(Paths.get("test.xml"));
-        } catch (IOException e) {
-        }
+        thrown.expect(IOException.class);
+        Files.delete(Paths.get("test.xml"));
+
     }
-    
+
 }
