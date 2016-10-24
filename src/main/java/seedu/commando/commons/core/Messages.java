0c610db411b630e423e26809966720bb12d3c5b1
Ckjr <conan.nma@gmail.com>
2016-10-25 00:47:06 +0800

    Add command examples

--- src/main/java/seedu/commando/commons/core/Messages.java
+++ src/main/java/seedu/commando/commons/core/Messages.java
@@ -1,14 +1,24 @@
 package seedu.commando.commons.core;
 
-import seedu.commando.logic.commands.RecallCommand;
-import seedu.commando.logic.commands.StoreCommand;
-
 import java.util.Optional;
+
+import seedu.commando.logic.commands.AddCommand;
+import seedu.commando.logic.commands.DeleteCommand;
+import seedu.commando.logic.commands.EditCommand;
+import seedu.commando.logic.commands.ExportCommand;
+import seedu.commando.logic.commands.FindCommand;
+import seedu.commando.logic.commands.FinishCommand;
+import seedu.commando.logic.commands.ImportCommand;
+import seedu.commando.logic.commands.RecallCommand;
+import seedu.commando.logic.commands.RedoCommand;
+import seedu.commando.logic.commands.StoreCommand;
+import seedu.commando.logic.commands.UnfinishCommand;
 
 /**
  * Container for user visible messages.
  */
 public class Messages {
+    
     public static final String UNKNOWN_COMMAND = "Unknown command: '%1$s'. Input 'help' for instructions on how to use this application.";
     public static final String UNKNOWN_COMMAND_FOR_HELP = "Unknown topic for help: '%1$s'.\n" + 
                                                           "Available Topics: add, edit, delete, find, clear, finish, unfinish, recall, undo, redo, faq, search logic, datetime formats, cheatsheet";
@@ -19,16 +29,20 @@ public class Messages {
     public static final String MISSING_TODO_DATERANGE_END = "Missing end date for date range of to-do.";
     public static final String INVALID_TODO_DATERANGE_START = "Invalid start date for date range of to-do.";
     public static final String INVALID_TODO_DATERANGE_END = "Invalid end date for date range of to-do.";
-    public static final String MISSING_TODO_ITEM_INDEX = "Missing to-do item index.";
+    public static final String MISSING_TODO_ITEM_INDEX = "Missing to-do item index.\n";
+    
     public static final String INDEXRANGE_CONSTRAINTS = "The end index must be equal or larger than the start index.";
     public static final String TODO_DATERANGE_END_MUST_AFTER_START = "The end date of the date range must be after start date.";
     public static final String TODO_DATERANGE_RECURRENCE_INVALID = "The recurrence for the given date range is not valid.";
-    public static final String MISSING_STORE_PATH = "Missing the path to store.";
+    
+    public static final String MISSING_STORE_PATH = "Missing the path to store. Sample usage include:";
+    public static final String MISSING_IMPORT_PATH = "Missing the path to import. Sample usage include:";
+    public static final String MISSING_EXPORT_PATH = "Missing the path to export. Sample usage include:";
+    
     public static final String MISSING_STORE_FILE = "Missing the file name to store.";
-    public static final String MISSING_EXPORT_PATH = "Missing the path to export.";
     public static final String MISSING_EXPORT_FILE = "Missing the file name to export.";
     public static final String MISSING_IMPORT_FILE = "Missing the file name to import.";
-    public static final String MISSING_IMPORT_PATH = "Missing the path to import.";
+    
     public static final String TODO_DUEDATE_CONSTRAINTS = "Due date can be any date.";
     public static final String TODO_CANNOT_HAVE_DUEDATE_AND_DATERANGE = "To-do cannot have both due date and date range!";
     public static final String TODO_ADDED = "To-do added: %1$s.";
@@ -37,6 +51,7 @@ public class Messages {
     public static final String TODO_NO_EDITS = "Nothing interesting happens.";
     public static final String TODO_NOT_FOUND = "To-do not found: %1$s";
     public static final String TODO_ITEM_INDEX_INVALID = "Invalid to-do index: %1$d.";
+    
     public static final String EXIT_APPLICATION = "Exiting application...";
     public static final String TODO_LIST_CLEARED = "Cleared to-do list!";
     public static final String HELP_WINDOW_SHOWN = "Opened help window.";
@@ -65,7 +80,17 @@ public class Messages {
     public static final String DELETE_COMMAND_NO_TAGS = "To-do with index '%1$s' has no tags to delete!";
     public static final String DELETE_COMMAND_NO_TIME_CONSTRAINTS = "To-do with index '%1$s' has no time constraints to delete!";
 
-    public static final String STORE_COMMAND_FORMAT = "Sample command: store ../../Dropbox/work/todolist.xml.";
+    public static final String STORE_COMMAND_FORMAT = ">> store ../../Dropbox/work/todolist.xml.";
+    public static final String IMPORT_COMMAND_FORMAT = ">> import ../../Dropbox/work/todolist.xml.";
+    public static final String EXPORT_COMMAND_FORMAT = ">> export ../../Dropbox/work/todolist.xml.";
+    public static final String ADD_COMMAND_FORMAT = ">> add watch ‘Fantastic Beast and where to find them’ #movie\n" + 
+                                                    ">> add Math Exam from 26 nov 1300h to 1430h\n";
+    public static final String DELETE_COMMAND_FORMAT = ">> delete 1";
+    public static final String FIND_COMMAND_FORMAT = ">> find\n" + ">> find thing #tag";
+    public static final String RECALL_COMMAND_FORMAT = ">> recall\n" + ">> recall thing #tag";
+    public static final String EDIT_COMMAND_FORMAT = ">> edit 1 change in description";
+    public static final String FINISH_COMMAND_FORMAT = ">> finish 1";
+    public static final String UNFINISH_COMMAND_FORMAT = ">> unfinish 1";
 
     /**
      * Returns an additional invalid command format message to be appended
@@ -75,9 +100,26 @@ public class Messages {
         switch (commandWord) {
             case StoreCommand.COMMAND_WORD:
                 return Optional.of(STORE_COMMAND_FORMAT);
-            default: break;
+            case ImportCommand.COMMAND_WORD:
+                return Optional.of(IMPORT_COMMAND_FORMAT);
+            case ExportCommand.COMMAND_WORD:
+                return Optional.of(EXPORT_COMMAND_FORMAT);
+            case AddCommand.COMMAND_WORD:
+                return Optional.of(ADD_COMMAND_FORMAT);
+            case DeleteCommand.COMMAND_WORD:
+                return Optional.of(DELETE_COMMAND_FORMAT);
+            case FindCommand.COMMAND_WORD:
+                return Optional.of(FIND_COMMAND_FORMAT);
+            case RecallCommand.COMMAND_WORD:
+                return Optional.of(RECALL_COMMAND_FORMAT);
+            case EditCommand.COMMAND_WORD:
+                return Optional.of(EDIT_COMMAND_FORMAT);
+            case FinishCommand.COMMAND_WORD:
+                return Optional.of(FINISH_COMMAND_FORMAT);
+            case UnfinishCommand.COMMAND_WORD:
+                return Optional.of(UNFINISH_COMMAND_FORMAT);
+            default: break; 
         }
-
         return Optional.empty();
     }
 }
