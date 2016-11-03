a4b0125c6d6fe5f3b4243d28d7945660d78beae4
nginyc <yunchuann@gmail.com>
2016-11-04 01:33:54 +0800

    Fix bug where tests would fail just after midnight

--- src/test/java/seedu/commando/model/ui/UiModelTest.java
+++ src/test/java/seedu/commando/model/ui/UiModelTest.java
@@ -1,24 +1,16 @@
 package seedu.commando.model.ui;
 
-import com.google.common.collect.Sets;
 import edu.emory.mathcs.backport.java.util.Collections;
 import org.junit.Before;
 import org.junit.Test;
 import seedu.commando.commons.exceptions.IllegalValueException;
 import seedu.commando.model.ToDoListManager;
-import seedu.commando.model.todo.ReadOnlyToDo;
 import seedu.commando.model.todo.ReadOnlyToDoList;
 import seedu.commando.model.todo.ToDoList;
-import seedu.commando.testutil.ToDoBuilder;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static seedu.commando.testutil.ToDoBuilder.*;
 
 //@@author A0139697H
@@ -35,16 +27,12 @@ public class UiModelTest {
             .add(TaskNewDueUnfinishedOldCreated)
             .add(TaskUnfinishedNewCreated)
             .add(TaskUnfinishedOldCreated)
-            .add(TaskTodayNewFinished)
-            .add(TaskTodayOldFinished)
             .add(TaskOldFinished)
             .add(TaskOlderFinished)
             .add(EventOldRangeUnfinishedNewCreated)
             .add(EventOldRangeUnfinishedOldCreated)
             .add(EventNewRangeUnfinishedNewCreated)
             .add(EventNewRangeUnfinishedOldCreated)
-            .add(EventTodayNewFinished)
-            .add(EventTodayOldFinished)
             .add(EventNewFinished)
             .add(EventOldFinished);
 
@@ -58,14 +46,12 @@ public class UiModelTest {
     public void getUiEvents_setup_orderCorrect() {
         List<UiToDo> events = uiModel.getEvents();
 
-        assertEquals(6, events.size());
+        assertEquals(4, events.size());
 
         assertEquals(EventOldRangeUnfinishedNewCreated, events.get(0));
         assertEquals(EventOldRangeUnfinishedOldCreated, events.get(1));
         assertEquals(EventNewRangeUnfinishedNewCreated, events.get(2));
         assertEquals(EventNewRangeUnfinishedOldCreated, events.get(3));
-        assertEquals(EventTodayNewFinished, events.get(4));
-        assertEquals(EventTodayOldFinished, events.get(5));
     }
 
     // Check if the order of events for ui is correct for history mode
@@ -74,12 +60,10 @@ public class UiModelTest {
         uiModel.setToDoListFilter(Collections.emptySet(), Collections.emptySet(), UiModel.FILTER_MODE.FINISHED);
         List<UiToDo> events = uiModel.getEvents();
 
-        assertEquals(4, events.size());
+        assertEquals(2, events.size());
 
-        assertEquals(EventTodayNewFinished, events.get(0));
-        assertEquals(EventTodayOldFinished, events.get(1));
-        assertEquals(EventNewFinished, events.get(2));
-        assertEquals(EventOldFinished, events.get(3));
+        assertEquals(EventNewFinished, events.get(0));
+        assertEquals(EventOldFinished, events.get(1));
     }
 
     // Check if the order of tasks for ui is correct
@@ -87,7 +71,7 @@ public class UiModelTest {
     public void getUiTasks_setup_orderCorrect() {
         List<UiToDo> tasks = uiModel.getTasks();
 
-        assertEquals(8, tasks.size());
+        assertEquals(6, tasks.size());
 
         assertEquals(TaskOldDueUnfinishedNewCreated, tasks.get(0));
         assertEquals(TaskOldDueUnfinishedOldCreated, tasks.get(1));
@@ -95,8 +79,6 @@ public class UiModelTest {
         assertEquals(TaskNewDueUnfinishedOldCreated, tasks.get(3));
         assertEquals(TaskUnfinishedNewCreated, tasks.get(4));
         assertEquals(TaskUnfinishedOldCreated, tasks.get(5));
-        assertEquals(TaskTodayNewFinished, tasks.get(6));
-        assertEquals(TaskTodayOldFinished, tasks.get(7));
     }
 
     // Check if the order of tasks for ui is correct for history
@@ -106,11 +88,9 @@ public class UiModelTest {
 
         List<UiToDo> tasks = uiModel.getTasks();
 
-        assertEquals(4, tasks.size());
+        assertEquals(2, tasks.size());
 
-        assertEquals(TaskTodayNewFinished, tasks.get(0));
-        assertEquals(TaskTodayOldFinished, tasks.get(1));
-        assertEquals(TaskOldFinished, tasks.get(2));
-        assertEquals(TaskOlderFinished, tasks.get(3));
+        assertEquals(TaskOldFinished, tasks.get(0));
+        assertEquals(TaskOlderFinished, tasks.get(1));
     }
 }
